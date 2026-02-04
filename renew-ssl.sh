79b10d223a3c3e6292d748e2533feb2bee1f68b3
mahmut0ff <klovvergg@gmail.com>
2026-02-04 23:39:43 +0600

    323

--- renew-ssl.sh
+++ renew-ssl.sh
@@ -3,5 +3,5 @@
 # Скрипт для обновления SSL сертификата
 # Добавь в cron: 0 3 * * * /path/to/project/renew-ssl.sh >> /var/log/certbot-renew.log 2>&1
 
-docker-compose run --rm certbot renew
-docker-compose exec nginx nginx -s reload
+docker compose run --rm certbot renew
+docker compose exec nginx nginx -s reload
